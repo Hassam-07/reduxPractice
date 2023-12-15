e14e206c786a33832c270c6c5bb74da45a607ece
Hassam <101381873+Hassam-07@users.noreply.github.com>
2023-12-15 14:33:31 +0500

    custom video player implementation completed

--- src/app/custom-video-player/custom-video-player.component.ts
+++ src/app/custom-video-player/custom-video-player.component.ts
@@ -1,4 +1,10 @@
-import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-custom-video-player',
@@ -10,13 +16,24 @@ export class CustomVideoPlayerComponent {
   @ViewChild('volumeSlider') volumeSlider!: ElementRef;
   @ViewChild('videoContainer') videoContainer!: ElementRef;
 
+  @ViewChild('muteButton') muteButton!: ElementRef;
+  @ViewChild('volumeHighIcon') volumeHighIcon!: ElementRef;
+  @ViewChild('volumeLowIcon') volumeLowIcon!: ElementRef;
+  @ViewChild('volumeMutedIcon') volumeMutedIcon!: ElementRef;
+  @ViewChild('timelineIndicator') timelineIndicator!: ElementRef;
+  // @ViewChild('timelineContainer') timelineContainer!: ElementRef;
+  @ViewChild('timeline', { static: true }) timeline!: ElementRef;
+  @ViewChild('currentTimeElem') currentTimeElem!: ElementRef;
+  @ViewChild('totalTimeElem') totalTimeElem!: ElementRef;
+
   isPlaying = false;
   volumeLevel = 'high';
-  playbackRates = [1, 1.25, 1.5, 2]; // Add more speeds if needed
+  playbackRates = [1, 1.25, 1.5, 2];
   currentPlaybackRateIndex = 0;
   currentTime = '0:00';
   duration = '0:00';
 
+  constructor(private renderer: Renderer2) {}
   ngAfterViewInit(): void {
     this.videoPlayer.nativeElement.addEventListener('timeupdate', () => {
       this.updateTimeDisplay();
@@ -30,6 +47,32 @@ export class CustomVideoPlayerComponent {
         this.videoPlayer.nativeElement.duration
       );
     });
+    // this.videoPlayer.nativeElement.addEventListener('timeupdate', () => {
+    //   this.currentTime = this.formatDuration(
+    //     this.videoPlayer.nativeElement.currentTime
+    //   );
+
+    //   const percent =
+    //     (this.videoPlayer.nativeElement.currentTime /
+    //       this.videoPlayer.nativeElement.duration) *
+    //     100;
+    //   this.renderer.setStyle(
+    //     this.timeline.nativeElement,
+    //     '--progress-position',
+    //     `${100 - percent}%`
+    //   );
+    // });
+  }
+  statusBarClick($event: MouseEvent) {
+    const el = $event.target as HTMLElement;
+    const clickX = $event.offsetX;
+    const totalWidth = el.offsetWidth;
+
+    const percentComplete = clickX / totalWidth;
+    const duration = this.videoPlayer.nativeElement.duration;
+
+    // Seek to the corresponding time
+    this.videoPlayer.nativeElement.currentTime = duration * percentComplete;
   }
 
   togglePlayPause(): void {
@@ -45,6 +88,25 @@ export class CustomVideoPlayerComponent {
   toggleMute(): void {
     this.videoPlayer.nativeElement.muted =
       !this.videoPlayer.nativeElement.muted;
+
+    this.toggleVolumeIcons();
+  }
+  private toggleVolumeIcons(): void {
+    const { volumeHighIcon, volumeLowIcon, volumeMutedIcon } = this;
+
+    if (this.videoPlayer.nativeElement.muted) {
+      // Display volume-muted-icon
+      this.renderer.setStyle(volumeMutedIcon.nativeElement, 'display', 'block');
+      // Hide volume-high-icon and volume-low-icon
+      this.renderer.setStyle(volumeHighIcon.nativeElement, 'display', 'none');
+      this.renderer.setStyle(volumeLowIcon.nativeElement, 'display', 'none');
+    } else {
+      // Display volume-high-icon
+      this.renderer.setStyle(volumeHighIcon.nativeElement, 'display', 'block');
+      // Hide volume-muted-icon and volume-low-icon
+      this.renderer.setStyle(volumeMutedIcon.nativeElement, 'display', 'none');
+      this.renderer.setStyle(volumeLowIcon.nativeElement, 'display', 'none');
+    }
   }
   @HostListener('input', ['$event'])
   onVolumeSliderChange(event: any): void {
@@ -62,36 +124,62 @@ export class CustomVideoPlayerComponent {
   }
 
   calculateVolumeLevel(): string {
-    if (
-      this.videoPlayer.nativeElement.muted ||
-      this.videoPlayer.nativeElement.volume === 0
-    ) {
-      this.volumeSlider.nativeElement.value = 0;
+    const volume = this.videoPlayer.nativeElement.volume;
+
+    if (this.videoPlayer.nativeElement.muted || volume === 0) {
+      // this.volumeSlider.nativeElement.value = '0';
+      this.toggleVolumeSliderIcons('muted');
       return 'muted';
-    } else if (this.videoPlayer.nativeElement.volume >= 0.5) {
+    } else if (volume >= 0.5) {
+      // this.volumeSlider.nativeElement.value = '0';
+      this.toggleVolumeSliderIcons('high');
       return 'high';
     } else {
+      this.toggleVolumeSliderIcons('low');
       return 'low';
     }
   }
 
-  // setVolume(volume: number): void {
-  //   this.videoPlayer.nativeElement.volume = volume;
-  //   this.updateVolumeIcon();
-  // }
+  toggleVolumeSliderIcons(level: string): void {
+    const { volumeHighIcon, volumeLowIcon, volumeMutedIcon } = this;
 
-  // updateVolumeIcon(): void {
-  //   if (
-  //     this.videoPlayer.nativeElement.muted ||
-  //     this.videoPlayer.nativeElement.volume === 0
-  //   ) {
-  //     this.volumeLevel = 'muted';
-  //   } else if (this.videoPlayer.nativeElement.volume >= 0.5) {
-  //     this.volumeLevel = 'high';
-  //   } else {
-  //     this.volumeLevel = 'low';
-  //   }
-  // }
+    switch (level) {
+      case 'muted':
+        this.renderer.setStyle(
+          volumeMutedIcon.nativeElement,
+          'display',
+          'block'
+        );
+        this.renderer.setStyle(volumeHighIcon.nativeElement, 'display', 'none');
+        this.renderer.setStyle(volumeLowIcon.nativeElement, 'display', 'none');
+        break;
+      case 'high':
+        this.renderer.setStyle(
+          volumeHighIcon.nativeElement,
+          'display',
+          'block'
+        );
+        this.renderer.setStyle(
+          volumeMutedIcon.nativeElement,
+          'display',
+          'none'
+        );
+        this.renderer.setStyle(volumeLowIcon.nativeElement, 'display', 'none');
+        break;
+      case 'low':
+        this.renderer.setStyle(volumeLowIcon.nativeElement, 'display', 'block');
+        this.renderer.setStyle(volumeHighIcon.nativeElement, 'display', 'none');
+        this.renderer.setStyle(
+          volumeMutedIcon.nativeElement,
+          'display',
+          'none'
+        );
+        break;
+      default:
+        break;
+    }
+  }
+
   changePlaybackSpeed(): void {
     this.currentPlaybackRateIndex =
       (this.currentPlaybackRateIndex + 1) % this.playbackRates.length;
@@ -107,6 +195,9 @@ export class CustomVideoPlayerComponent {
     const minutes = Math.floor(time / 60);
     const seconds = Math.floor(time % 60);
     return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+  }
+  skip(duration: any): void {
+    this.videoPlayer.nativeElement.currentTime += duration;
   }
 
   toggleFullScreen(): void {
